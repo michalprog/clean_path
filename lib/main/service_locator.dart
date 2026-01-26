6e525dc5681984fff4d1565fe7eef01d17fed9ce
michalprog <michalch001@gmail.com>
2026-01-26 20:37:05 +0100

    working leveling

--- lib/main/service_locator.dart
+++ lib/main/service_locator.dart
@@ -3,6 +3,7 @@ import 'package:shared_preferences/shared_preferences.dart';
 import 'package:sqflite/sqflite.dart';
 import '/data_types/leveling_service.dart';
 import '/sqlflite/database_manager.dart';
+import '/providers/account_provider.dart';
 import '/providers/settings_storage.dart';
 
 final GetIt getIt = GetIt.instance;
@@ -10,20 +11,24 @@ final GetIt getIt = GetIt.instance;
 Future<void> setupServiceLocator() async {
   getIt.registerLazySingleton<DatabaseManager>(() => DatabaseManager());
   getIt.registerSingletonAsync<Database>(
-    () => getIt<DatabaseManager>().database,
+        () => getIt<DatabaseManager>().database,
   );
 
   getIt.registerSingletonAsync<SharedPreferences>(
-    () => SharedPreferences.getInstance(),
+        () => SharedPreferences.getInstance(),
   );
 
   getIt.registerSingletonWithDependencies<SettingsStorage>(
-    () => SettingsStorage(getIt<SharedPreferences>()),
+        () => SettingsStorage(getIt<SharedPreferences>()),
     dependsOn: [SharedPreferences],
   );
 
   getIt.registerLazySingleton<LevelingService>(
-    () => const DefaultLevelingService(),
+        () => const DefaultLevelingService(),
+  );
+
+  getIt.registerLazySingleton<AccountProvider>(
+        () => AccountProvider(),
   );
 
   await getIt.allReady();
